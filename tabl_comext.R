473dbff95f8e078985d482887dc488efdba430d3
Rousset Clément <C0764J@ad.insee.intra>
2019-02-08 16:48:20 +0100

    ajout des comparaisons omises fiches france

--- tabl_comext.R
+++ tabl_comext.R
@@ -21,11 +21,11 @@ charge<<-function(file,acharger=NULL){load(file)
   
 }
 row1 <-  ligne_date()
-row2 <-f_row("Exportations","p6_d_7ch",VT,VA,b_fr,bold=T,dig=dig)
-row3 <-f_row("dont Produits manufactures","p6_dim_7ch",VT,VA,b_fr,dig=dig)
-row4 <-f_row("Importations","p7_d_7ch",VT,VA,b_fr,bold=T,dig=dig)
-row5 <-f_row("dont Produits manufactures","p7_dim_7ch",VT,VA,b_fr,dig=dig)
-row6 <-f_row("Contribution du commerce exterieur","c.solde_d_7ch",NIV,NIV_A,b_fr,bold=T,dig=dig)
+row2 <-f_row("Exportations","p6_d_7ch",VT,VA,b_fr,b_fr_old,bold=T,dig=dig)
+row3 <-f_row("dont Produits manufactures","p6_dim_7ch",VT,VA,b_fr,b_fr_old,dig=dig)
+row4 <-f_row("Importations","p7_d_7ch",VT,VA,b_fr,b_fr_old,bold=T,dig=dig)
+row5 <-f_row("dont Produits manufactures","p7_dim_7ch",VT,VA,b_fr,b_fr_old,dig=dig)
+row6 <-f_row("Contribution du commerce exterieur","c.solde_d_7ch",NIV,NIV_A,b_fr,b_fr_old,bold=T,dig=dig)
 
 tabl_ext<-paste("<h1>Prevision de croissance des echanges exterieurs</h1><table style=\"width:100%\" border=1>",row1,row2,row3,row4,row5,row6,"</table>",sep="")
 
--- tabl_pibfr.R
+++ tabl_pibfr.R
@@ -5,16 +5,16 @@ tabl_pibfr<-function(dig=1){
     
   }
   row1 <-  ligne_date()
-  row2 <-f_row("Produit interieur brut","pib_7ch",VT,VA,b_fr,dig=dig,bold=T)
-  row3 <-f_row("Consommation privee","p3m_d_7ch",VT,VA,b_fr,dig=dig)
-  row4 <-f_row("Investissement","p51_d_7ch",VT,VA,b_fr,dig=dig)
-  row5 <-f_row("Consommation publique","p3pg_d_7ch",VT,VA,b_fr,dig=dig)
-  row6 <-f_row("Exportations","p6_d_7ch",VT,VA,b_fr,dig=dig)
-  row7 <-f_row("Importations","p7_d_7ch",VT,VA,b_fr,dig=dig)
+  row2 <-f_row("Produit interieur brut","pib_7ch",VT,VA,b_fr,b_fr_old,dig=dig,bold=T)
+  row3 <-f_row("Consommation privee","p3m_d_7ch",VT,VA,b_fr,b_fr_old,dig=dig)
+  row4 <-f_row("Investissement","p51_d_7ch",VT,VA,b_fr,b_fr_old,dig=dig)
+  row5 <-f_row("Consommation publique","p3pg_d_7ch",VT,VA,b_fr,b_fr_old,dig=dig)
+  row6 <-f_row("Exportations","p6_d_7ch",VT,VA,b_fr,b_fr_old,dig=dig)
+  row7 <-f_row("Importations","p7_d_7ch",VT,VA,b_fr,b_fr_old,dig=dig)
   row8<-"<tr><td><b>Contributions</b></td></tr>"
-  row9 <-f_row("Demande interieure hors stocks","c.dintfhs_d_7ch",NIV,NIV_A,b_fr,dig=dig)
-  row10 <-f_row("Variations de stocks","c.p54_d_7ch",NIV,NIV_A,b_fr,dig=dig)
-  row11 <-f_row("Commerce exterieure","c.solde_d_7ch",NIV,NIV_A,b_fr,dig=dig)
+  row9 <-f_row("Demande interieure hors stocks","c.dintfhs_d_7ch",NIV,NIV_A,b_fr,b_fr_old,dig=dig)
+  row10 <-f_row("Variations de stocks","c.p54_d_7ch",NIV,NIV_A,b_fr,b_fr_old,dig=dig)
+  row11 <-f_row("Commerce exterieure","c.solde_d_7ch",NIV,NIV_A,b_fr,b_fr_old,dig=dig)
  
   
 tabl<-paste("<h1>Fiche de PIB France</h1><table style=\"width:100%\" border=1>",row1,row2,row3,row4,row5,row6,row7,row8,row9,row10,row11,"</table>",sep="")
