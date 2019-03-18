0311bf6ddac4672ee003aae1de91306d05b9986c
Clément Rousset <clement.rousset@insee.fr>
2019-03-18 11:07:01 +0100

    raccourcis

---
+++ Appli_MB.bat
@@ -0,0 +1,1 @@
+"D:\S6H2M1\Mes Documents\R\R-3.4.3\bin\Rscript.exe" -e "shiny::runApp('N:/GDCJ/N-GDCJ/Echanges.DCJ/DSC/Rshiny chiffres de la note', launch.browser = TRUE)"
---
+++ Appli_MF.bat
@@ -0,0 +1,1 @@
+"d:\DO5ZMM\MESDOC~1\R\R-34~1.1\bin\Rscript.exe" -e "shiny::runApp('N:/GDCJ/N-GDCJ/Echanges.DCJ/DSC/Rshiny chiffres de la note', launch.browser = TRUE)"
